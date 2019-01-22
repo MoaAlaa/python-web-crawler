21ba8d4f8e40346c4145403fb8f1d2293ba585d7
moaalaa <moaalaa16@gmail.com>
2019-01-22 16:13:33 +0200

    WIP:Implementing Spiders

---
+++ spider.py
@@ -0,0 +1,39 @@
+from urllib.request import urlopen
+
+from general import *
+from links_finder import LinksFinder
+
+
+class Spider:
+
+    # Class Variables (Shared Among All Instances)
+    project_name = ''
+    base_url = ''
+    domain_name = ''
+    queue_file = ''
+    crawled_file = ''
+    queue = set()
+    crawled = set()
+
+    def __init__(self, project_name, base_url, domain_name):
+        Spider.project_name = project_name
+        Spider.base_url = base_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = Spider.project_name + '/queue.txt'
+        Spider.crawled_file = Spider.project_name + '/crawled.txt'
+        self.boot()
+        self.crawl_page('First Spider', Spider.base_url)
+    
+    @staticmethod
+    def boot():
+        create_project_dir(Spider.project_name)
+        create_data_files(Spider.project_name, Spider.base_url)
+        Spider.queue = file_to_set(Spider.queue_file)
+        Spider.crawled = file_to_set(Spider.crawled_file)
+
+    @staticmethod
+    def crawl_page(thread_name, page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name + ' Now Crawling ' + page_url)
+            print('::Queue ' + str(len(Spider.queue)))
+            print('::crawled ' + str(len(Spider.crawled)))
