1bc72ff5f609634e800cdc284d818cbdfc29e7a3
moaalaa <moaalaa16@gmail.com>
2019-01-22 16:56:46 +0200

    Implementing Jobs

--- main.py
+++ main.py
@@ -13,3 +13,19 @@ CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
 NUMBER_OF_THREADS = 8
 queue = Queue()
 Spider(PROJECT_NAME, HOME_PAGE, DOMAIN_NAME)
+
+# Each Queue Links Is A New Job
+def create_jobs():
+    for link in file_to_set(QUEUE_FILE):
+        queue.put(link)
+    queue.join()
+    crawl()
+
+
+# Check If There Are Items In The Queue, If So Crawl Them
+def crawl():
+    queue_links = file_to_set(QUEUE_FILE)
+
+    if len(queue_links) > 0:
+        print(str(len(queue_links)) + " Links In The Queue")
+        create_jobs()
