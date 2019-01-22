12ed3d8a2fc5af5f8c13eec11949569b9ec318a4
moaalaa <moaalaa16@gmail.com>
2019-01-22 17:10:13 +0200

    finishing

--- main.py
+++ main.py
@@ -14,6 +14,23 @@ NUMBER_OF_THREADS = 8
 queue = Queue()
 Spider(PROJECT_NAME, HOME_PAGE, DOMAIN_NAME)
 
+
+# Create Worker Threads (Will Die When Main Exit)
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
+
+
+# Do The Next Job In The Queue
+def work():
+    while True:
+        url = queue.get()
+        Spider.crawl_page(threading.current_thread().name, url)
+        queue.task_done() 
+
+
 # Each Queue Links Is A New Job
 def create_jobs():
     for link in file_to_set(QUEUE_FILE):
@@ -29,3 +46,7 @@ def crawl():
     if len(queue_links) > 0:
         print(str(len(queue_links)) + " Links In The Queue")
         create_jobs()
+
+
+create_workers()
+crawl()
